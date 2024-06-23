b1e5828b7a06912dab8576880af83913c67ed967
waldohidalgo <whidalgohp@gmail.com>
2024-06-22 21:30:16 -0400

    Revert "simplifico los tests"
    This reverts commit 41e259457b4e47222fd03016bfcd098cbf560fef.

--- README.md
+++ README.md
@@ -81,17 +81,18 @@ El registro de IPs se realiza en una collection de una base de datos MongoDB enc
 ### 1.Viewing one stock: GET request to /api/stock-prices/
 
 ```js
+let likes = null;
 test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
   chai
     .request(server)
     .keepOpen()
     .get("/api/stock-prices?stock=GOOG")
     .end(function (err, res) {
+      likes = res.body.stockData.likes;
       assert.equal(res.status, 200);
       assert.isString(res.body.stockData.stock);
       assert.isNumber(res.body.stockData.price);
       assert.isNumber(res.body.stockData.likes);
-
       done();
     });
 });
@@ -104,16 +105,31 @@ test("Viewing one stock and liking it: GET request to /api/stock-prices/", funct
   chai
     .request(server)
     .keepOpen()
-    .get("/api/stock-prices?stock=GOOG&like=true")
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.isString(res.body.stockData.stock);
-      assert.isNumber(res.body.stockData.price);
-      assert.isNumber(res.body.stockData.likes);
+    .get("/api/ip")
+    .end(async (err, res) => {
+      const id = res.body.id;
 
-      assert.equal(1, res.body.stockData.likes);
+      const ipsCollection = dbObject.db.collection("ips");
+      if (id) {
+        await ipsCollection.deleteOne({
+          _id: new ObjectId(String(id)),
+        });
+      }
 
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .get("/api/stock-prices?stock=GOOG&like=true")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isString(res.body.stockData.stock);
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          // likes should be increased by 1
+          assert.equal(likes + 1, res.body.stockData.likes);
+          likes = likes + 1;
+          done();
+        });
     });
 });
 ```
@@ -132,7 +148,7 @@ test("Viewing the same stock and liking it again: GET request to /api/stock-pric
       assert.isNumber(res.body.stockData.price);
       assert.isNumber(res.body.stockData.likes);
       // likes should be the same
-      assert.equal(1, res.body.stockData.likes);
+      assert.equal(likes, res.body.stockData.likes);
       done();
     });
 });
@@ -141,24 +157,45 @@ test("Viewing the same stock and liking it again: GET request to /api/stock-pric
 ### 4.Viewing two stocks: GET request to /api/stock-prices/
 
 ```js
+let likes1 = null;
+let likes2 = null;
+
 test("Viewing two stocks: GET request to /api/stock-prices/", function (done) {
   chai
     .request(server)
     .keepOpen()
-    .get("/api/stock-prices?stock=AAPL&stock=MSFT")
+    .get("/api/stock-prices?stock=GOOG")
     .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.isObject(res.body.stockData[0]);
-      assert.isObject(res.body.stockData[1]);
+      likes1 = res.body.stockData.likes;
 
-      assert.property(res.body.stockData[0], "rel_likes");
-      assert.property(res.body.stockData[1], "rel_likes");
-      assert.equal(
-        res.body.stockData[0]["rel_likes"] + res.body.stockData[1]["rel_likes"],
-        0
-      );
+      chai
+        .request(server)
+        .keepOpen()
+        .get("/api/stock-prices?stock=MSFT")
+        .end(function (err, res) {
+          likes2 = res.body.stockData.likes;
 
-      done();
+          chai
+            .request(server)
+            .keepOpen()
+            .get("/api/stock-prices?stock=GOOG&stock=MSFT")
+            .end(function (err, res) {
+              assert.equal(res.status, 200);
+              assert.isObject(res.body.stockData[0]);
+              assert.isObject(res.body.stockData[1]);
+
+              assert.property(res.body.stockData[0], "rel_likes");
+              assert.property(res.body.stockData[1], "rel_likes");
+              assert.equal(
+                res.body.stockData[0]["rel_likes"] +
+                  res.body.stockData[1]["rel_likes"],
+                0
+              );
+
+              assert.equal(likes1 - likes2, res.body.stockData[0]["rel_likes"]);
+              done();
+            });
+        });
     });
 });
 ```
@@ -170,19 +207,34 @@ test("Viewing two stocks and liking them: GET request to /api/stock-prices/", fu
   chai
     .request(server)
     .keepOpen()
-    .get("/api/stock-prices?stock=AAPL&stock=MSFT&like=true")
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      const stockData1 = res.body.stockData[0];
-      const stockData2 = res.body.stockData[1];
-      assert.isObject(stockData1);
-      assert.isObject(stockData2);
+    .get("/api/ip")
+    .end(async (err, res) => {
+      const id = res.body.id;
 
-      assert.property(stockData1, "rel_likes");
-      assert.property(stockData2, "rel_likes");
-      assert.equal(stockData1["rel_likes"] + stockData2["rel_likes"], 0);
+      const ipsCollection = dbObject.db.collection("ips");
+      if (id) {
+        await ipsCollection.deleteOne({
+          _id: new ObjectId(String(id)),
+        });
+      }
 
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .get("/api/stock-prices?stock=GOOG&stock=MSFT&like=true")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          const stockData1 = res.body.stockData[0];
+          const stockData2 = res.body.stockData[1];
+          assert.isObject(stockData1);
+          assert.isObject(stockData2);
+
+          assert.property(stockData1, "rel_likes");
+          assert.property(stockData2, "rel_likes");
+          assert.equal(stockData1["rel_likes"] + stockData2["rel_likes"], 0);
+          assert.equal(likes1 - likes2, stockData1["rel_likes"]);
+          done();
+        });
     });
 });
 ```
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,17 +10,19 @@ chai.use(chaiHttp);
 suite("Functional Tests", function () {
   // Viewing one stock: GET request to /api/stock-prices/
 
+  let likes = null;
+
   test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
     chai
       .request(server)
       .keepOpen()
       .get("/api/stock-prices?stock=GOOG")
       .end(function (err, res) {
+        likes = res.body.stockData.likes;
         assert.equal(res.status, 200);
         assert.isString(res.body.stockData.stock);
         assert.isNumber(res.body.stockData.price);
         assert.isNumber(res.body.stockData.likes);
-
         done();
       });
   });
@@ -31,16 +33,31 @@ suite("Functional Tests", function () {
     chai
       .request(server)
       .keepOpen()
-      .get("/api/stock-prices?stock=GOOG&like=true")
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isString(res.body.stockData.stock);
-        assert.isNumber(res.body.stockData.price);
-        assert.isNumber(res.body.stockData.likes);
+      .get("/api/ip")
+      .end(async (err, res) => {
+        const id = res.body.id;
 
-        assert.equal(1, res.body.stockData.likes);
+        const ipsCollection = dbObject.db.collection("ips");
+        if (id) {
+          await ipsCollection.deleteOne({
+            _id: new ObjectId(String(id)),
+          });
+        }
 
-        done();
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/stock-prices?stock=GOOG&like=true")
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isString(res.body.stockData.stock);
+            assert.isNumber(res.body.stockData.price);
+            assert.isNumber(res.body.stockData.likes);
+            // likes should be increased by 1
+            assert.equal(likes + 1, res.body.stockData.likes);
+            likes = likes + 1;
+            done();
+          });
       });
   });
 
@@ -57,31 +74,54 @@ suite("Functional Tests", function () {
         assert.isNumber(res.body.stockData.price);
         assert.isNumber(res.body.stockData.likes);
         // likes should be the same
-        assert.equal(1, res.body.stockData.likes);
+        assert.equal(likes, res.body.stockData.likes);
         done();
       });
   });
   // Viewing two stocks: GET request to /api/stock-prices/
 
+  let likes1 = null;
+  let likes2 = null;
+
   test("Viewing two stocks: GET request to /api/stock-prices/", function (done) {
     chai
       .request(server)
       .keepOpen()
-      .get("/api/stock-prices?stock=AAPL&stock=MSFT")
+      .get("/api/stock-prices?stock=GOOG")
       .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body.stockData[0]);
-        assert.isObject(res.body.stockData[1]);
+        likes1 = res.body.stockData.likes;
 
-        assert.property(res.body.stockData[0], "rel_likes");
-        assert.property(res.body.stockData[1], "rel_likes");
-        assert.equal(
-          res.body.stockData[0]["rel_likes"] +
-            res.body.stockData[1]["rel_likes"],
-          0
-        );
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/stock-prices?stock=MSFT")
+          .end(function (err, res) {
+            likes2 = res.body.stockData.likes;
 
-        done();
+            chai
+              .request(server)
+              .keepOpen()
+              .get("/api/stock-prices?stock=GOOG&stock=MSFT")
+              .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.isObject(res.body.stockData[0]);
+                assert.isObject(res.body.stockData[1]);
+
+                assert.property(res.body.stockData[0], "rel_likes");
+                assert.property(res.body.stockData[1], "rel_likes");
+                assert.equal(
+                  res.body.stockData[0]["rel_likes"] +
+                    res.body.stockData[1]["rel_likes"],
+                  0
+                );
+
+                assert.equal(
+                  likes1 - likes2,
+                  res.body.stockData[0]["rel_likes"]
+                );
+                done();
+              });
+          });
       });
   });
 
@@ -91,19 +131,34 @@ suite("Functional Tests", function () {
     chai
       .request(server)
       .keepOpen()
-      .get("/api/stock-prices?stock=AAPL&stock=MSFT&like=true")
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        const stockData1 = res.body.stockData[0];
-        const stockData2 = res.body.stockData[1];
-        assert.isObject(stockData1);
-        assert.isObject(stockData2);
+      .get("/api/ip")
+      .end(async (err, res) => {
+        const id = res.body.id;
 
-        assert.property(stockData1, "rel_likes");
-        assert.property(stockData2, "rel_likes");
-        assert.equal(stockData1["rel_likes"] + stockData2["rel_likes"], 0);
+        const ipsCollection = dbObject.db.collection("ips");
+        if (id) {
+          await ipsCollection.deleteOne({
+            _id: new ObjectId(String(id)),
+          });
+        }
 
-        done();
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/stock-prices?stock=GOOG&stock=MSFT&like=true")
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            const stockData1 = res.body.stockData[0];
+            const stockData2 = res.body.stockData[1];
+            assert.isObject(stockData1);
+            assert.isObject(stockData2);
+
+            assert.property(stockData1, "rel_likes");
+            assert.property(stockData2, "rel_likes");
+            assert.equal(stockData1["rel_likes"] + stockData2["rel_likes"], 0);
+            assert.equal(likes1 - likes2, stockData1["rel_likes"]);
+            done();
+          });
       });
   });
 });
